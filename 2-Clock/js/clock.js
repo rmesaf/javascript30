69e84bbf641c45ad870b41ba5edaae6abcf9a4f9
Ricardo Mesa <rmesaf@gmail.com>
2022-12-08 12:04:49 -0500

    Digital clock

--- 2-Clock/js/clock.js
+++ 2-Clock/js/clock.js
@@ -3,6 +3,9 @@
   const secondsHand = document.querySelector('.seconds');
   const minutesHand = document.querySelector('.minutes');
   const hoursHand = document.querySelector('.hours');
+  const secondsDigital = document.querySelector('.digital-seconds');
+  const minutesDigital = document.querySelector('.digital-minutes');
+  const hoursDigital = document.querySelector('.digital-hours');
 
   function getDegrees(time, type) {
     const getConversionRate = {
@@ -14,17 +17,28 @@
     return ((Number(time) * 360) / getConversionRate[type]) + 90;
   }
 
+  function setAnalog(hours, minutes, seconds) {
+    const secondDegrees = getDegrees(seconds, 'seconds');
+    secondsHand.style.transform = `rotate(${secondDegrees}deg)`;
+    const minutesDegrees = getDegrees(minutes, 'minutes');
+    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    const hoursDegrees = getDegrees(hours, 'hours');
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  }
+
+  function setDigital(hours, minutes, seconds) {
+    secondsDigital.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+    minutesDigital.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+    hoursDigital.innerHTML = hours < 10 ? `0${hours}` : hours;
+  }
+
   function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondDegrees = getDegrees(seconds, 'seconds');
-    secondsHand.style.transform = `rotate(${secondDegrees}deg)`;
     const minutes = now.getMinutes();
-    const minutesDegrees = getDegrees(minutes, 'minutes');
-    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
-    const hours = now.getHours()
-    const hoursDegrees = getDegrees(hours, 'hours');
-    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    const hours = now.getHours();
+    setAnalog(hours, minutes, seconds);
+    setDigital(hours, minutes, seconds);
   }
 
   setInterval(setDate, 1000);
